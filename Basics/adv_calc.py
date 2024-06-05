ef905aa57a29eb57815dfbd797ad5b8d74529e60
Shreyas Ladhe <112794761+ShreyasLadhe@users.noreply.github.com>
2024-06-05 20:27:56 +0530

    Added more files to Basic folder

---
+++ Basics/adv_calc.py
@@ -0,0 +1,14 @@
+import calculator as basic_calc
+
+num_1 = 10
+num_2 = 3
+
+basic_calc.multiply
+
+def mod(x, y):
+    mod = num_1 % num_2
+    return mod
+
+t = mod(num_1, num_2)
+
+print(t)
---
+++ Basics/calculator.py
@@ -0,0 +1,28 @@
+num_1 = 10
+num_2 = 2
+
+def addition(a, b):
+    add = num_1 + num_2
+    return add
+
+def subtraction(a, b):
+    sub = num_1 - num_2
+    return sub
+
+def multiply(a, b):
+    mul = num_1 * num_2
+    return mul
+
+def divide(a, b):
+    div = num_1 / num_2
+    return div
+
+p = addition(num_1, num_2)
+q = subtraction(num_1, num_2)
+r = multiply(num_1, num_2)
+s = divide(num_1, num_2)
+
+print(p)
+print(q)
+print(r)
+print(s)
